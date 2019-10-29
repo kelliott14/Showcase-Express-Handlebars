47156db7a50c7e39238436d3c0790971466f9e1a
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-10-29 11:41:36 +1100

    server is connecting

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
-var mysql = require("mysql")
+var mySQL = require("mysql")
 
 var connection = mySQL.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password : "RickyPonting14",
-    database : 'burgers'
+    database : 'burgers_db'
   });
+
+  connection.connect(function(err){
+      if(err) {
+          console.log("error connection: " + err.stack);
+          return
+      }
+      console.log("connected as id " + connection.threadId);
+  })
 
   module.exports = connection;
